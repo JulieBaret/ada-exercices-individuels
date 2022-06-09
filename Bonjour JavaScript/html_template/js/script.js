628364146ada0cb13987781e89129494b108e15c
Julie Baret <jem.baret@gmail.com>
2022-06-09 10:14:24 +0200

    Etape 2

--- Bonjour JavaScript/html_template/js/script.js
+++ Bonjour JavaScript/html_template/js/script.js
@@ -1,1 +1,9 @@
+// let userName = prompt("Quel est ton nom ?")
+// let welcome = "Bonjour " + userName
 
+let welcomeUser = "\ud83d\udc4b Bonjour " + prompt("Quel est ton nom ?");
+document.body.innerHTML += `<h2>${welcomeUser}</h2>`;
+
+let ageUser = new Date().getFullYear() - parseInt(prompt("En quelle année es-tu née ?"));
+let welcomeAgeUser = `Vous avez ${ageUser} ans.`;
+document.body.innerHTML += `<h3>${welcomeAgeUser}</h3>`;
