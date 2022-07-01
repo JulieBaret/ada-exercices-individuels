64db66bb84961b9c8088054d082f3bd42e23710b
Julie Baret <jem.baret@gmail.com>
2022-07-01 16:13:46 +0200

    etape 4 en cours...

--- Guess a number/script.js
+++ Guess a number/script.js
@@ -1,4 +1,4 @@
-//etape 1 : on a créé une variable givenNumber et une fonction askNumber qui enregistre la reponse au prompteur dans givenNumber en Int
+//etape 1 : variable givenNumber + fonction askNumber qui stocke la reponse au prompt
 let givenNumber;
 function askNumber(){
     givenNumber = parseInt(prompt("Saisissez un chiffre"));
@@ -7,12 +7,9 @@ function askNumber(){
 
 }
 
-// askNumber();
-console.log(givenNumber);
-
-// etape 2 : on a créé une fonction didIWin qui répond en alert selon le givenNumber 
-function didIWin(givenNumber){
-//     if(givenNumber < 22){
+// etape 2 : fonction didIWin qui répond en alert selon le givenNumber + fonction gamePlay qui appelle
+//function didIWin(givenNumber){
+    //     if(givenNumber < 22){
 //         alert("Plus grand");
 //         gamePlay();
 //     }
@@ -28,6 +25,15 @@ function didIWin(givenNumber){
 //         gamePlay();
 //     }
 // }
+// function gamePlay(){
+//     askNumber();
+//     didIWin(givenNumber);
+// }
+//gamePlay();
+
+//etape 3 : didIwin return true ou false
+function didIWin(givenNumber){
+
     if(givenNumber == 22){
         console.log("True");
         return true;
@@ -37,22 +43,71 @@ function didIWin(givenNumber){
     }
 }
 
-// didIWin(givenNumber);
+//etape 3 : while tant que givenNumber n'est pas 22 ; version longue
+//function gamePlay(){
+    //     let victory;
+//     do{
+//         let numberByUser = askNumber();
+//         victory = didIWin(numberByUser);
+//             if(victory == true){
+//                 alert("Congratulations!");
+//             }
+//             else{
+//                 alert("Try again...");
+//             }
+//     } while(victory == false);
+// }
 
-//etape 3 
+//etape 3 : version courte
 function gamePlay(){
-    let victory;
-    do{
-        let numberByUser = askNumber();
-        victory = didIWin(numberByUser);
-            if(victory == true){
+do{
+        askNumber();
+        didIWin(givenNumber);
+            if(didIWin(givenNumber)){
                 alert("Congratulations!");
             }
             else{
                 alert("Try again...");
             }
-        
-    } while(victory == false);
+    } while(didIWin(givenNumber) == false);
 }
 
-gamePlay();
+//gamePlay();
+
+
+//etape 4 : BOUCLE INFINIE...
+
+
+function didIReallyWin(a,b){
+
+    if(a == b){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function playerOne() {
+
+let givenNumberOne;
+let tentatives = 0;
+
+do{ 
+    givenNumberOne = prompt("Joueur 1, saisissez un chiffre entre 0 et 50 :");
+        if (givenNumberOne < 0 || givenNumberOne > 50){
+        tentatives+=1
+        playerOne();
+        };
+    didIReallyWin(givenNumberOne, 22);
+        if(didIReallyWin(givenNumberOne, 22)){
+            alert("Bravo ! Tu as deviné le nombre !");
+        } else {
+            alert("Essaie encore...")
+            tentatives+=1;
+        }
+} while(didIReallyWin(givenNumberOne, 22) == false);
+}
+
+
+playerOne();
