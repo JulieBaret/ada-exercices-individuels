27b4507bc90b10e2f99a6e56449872cc9a129131
Julie Baret <jem.baret@gmail.com>
2022-07-07 16:49:40 +0200

    Exercice individuel à faire

---
+++ Parlez-vous le morse/script.js
@@ -0,0 +1,10 @@
+//Etape 1 : convertir une string en tableau
+function getLatinCharacterList(texte){
+    return texte.split('');
+}
+console.log(getLatinCharacterList("hello world"));
+
+//Etape 2 : 
+function translateLatinCharacter(a){
+
+}
